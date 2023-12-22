22c6137e38378d4d92b7872785e448cf8cab1491
SER <104014082+SERS-S@users.noreply.github.com>
2023-12-22 11:59:18 +0200

    rm

--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 chmod +x runtcpserver.sh
 echo -e "\033[1m\033[94mThe first file is compiled\033[0m"
-sleep 0.5
+sleep 0.75
 chmod +x runtcpclient.sh
 echo -e "\033[1m\033[94mThe second file is compiled\033[0m"
-sleep 0.5
+sleep 0.75
 chmod +x transmission/GenKey/genK.sh
 echo -e "\033[1m\033[94mThe third file is compiled\033[0m"
-sleep 0.5
+sleep 0.75
 chmod +x transmission/GenKey/encryptK.sh
 echo -e "\033[1m\033[94mThe fourth file is compiled\033[0m"
-sleep 0.5
+sleep 0.75
 chmod +x transmission/GenKey/decryptK.sh
 echo -e "\033[1m\033[94mThe fifth file is compiled\033[0m"
-sleep 0.5
+sleep 0.75
 cd transmission && cd tcpclient && cargo check && cd .. && cd tcpserver && cargo check
 echo -e "\033[1m\033[94mThe cargo environment is configured\033[0m"
-sleep 0.5
+sleep 0.75
 cd .. && cd GenKey && python3 -m venv venv && source venv/bin/activate && pip install --upgrade pip && pip install pycryptodome
 echo -e "\033[1m\033[94mThe python environment is configured\033[0m"
+rm -- "$0"
